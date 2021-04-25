cd9a16f94afaa99a3c0d8cec3b2fd4f4daa97a32
DXGGX <63854697+DXGGX@users.noreply.github.com>
2021-04-25 14:37:18 +0300

    asetus sivu lisätty
    puuttuu toimivuudet poistamisesta :///

---
+++ asetukset.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>
+		TeHoChat
+	</title>
+	<meta name="keywords" content="" />
+	<meta name="description" content="" />
+	<link href="css/asetukset.css" rel="stylesheet" type="text/css" media="all" />
+	
+</head>
+<body>
+<div id="header-wrapper">
+	<div id="header" class="container">
+		<div id="logo">
+			<span class="icon icon-group"></span>
+			<h1>
+				<a href="#">
+					TeHoChat
+				</a>
+			</h1>
+			<?php
+				include("includes/iheader.php");
+				include("includes/inavIndex.php");
+			?>
+		</div>
+		<div id="triangle-up">		
+		</div>
+	</div>
+</div>
+<div id="menu-wrapper">
+		<div id="menu">
+			<ul>
+				<li><a href="userChat.php" accesskey="2" title="">Aloita chat</a></li>
+				<li><a href="#" accesskey="3" title="">HRV</a></li>
+				<li><a href="asetukset.php" accesskey="5" title="">Asetukset</a></li>
+			</ul>
+		</div>
+</div>
+<div id="wrapper">
+	<div id="featured-wrapper">
+	
+		<div class="extra2 container">
+			<div class="ebox1">
+				
+				<div class="asetukset-main">
+					<h2>ASETUKSET</h2>
+                    
+                    <br>
+                    <div class="asetukset-teksti">
+                        <p> POISTA KÄYTTTÄJÄ </P>
+
+                        <p> POISTA SYKEVÄLIDATA </p>
+                    </div>
+
+
+				</div>
+				
+				
+			</div>		
+
+
+		</div>	
+
+	</div>
+</div>
+
+
+</div>
+
+
+
+
+
+
+</body>
+
+
+</html>
